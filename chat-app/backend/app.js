c88dbfdc5a0a82131b8cbca61948c8d8d885d08b
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-06-23 18:35:18 +0100

    Implement WebSocket server and broadcast messages to connected clients

--- chat-app/backend/app.js
+++ chat-app/backend/app.js
@@ -1,3 +1,4 @@
+const WebSocket = require("ws");
 const express = require("express");
 const app = express();
 app.use(express.json())
@@ -13,11 +14,12 @@ const fetchAllMessages = () => {
     return allMessages
 }
 
-app.get("/message", (req, res) => {
+app.get("/messages", (req, res) => {
     const messages = fetchAllMessages()
     res.json(messages)
 })
 
+const clients = [];
 app.post("/message", (req, res) => {
 
     const {message, name} = req.body
@@ -26,6 +28,14 @@ app.post("/message", (req, res) => {
          name,message
     })
     console.log(allMessages)
+
+    clients.forEach(client =>{
+        client.send(JSON.stringify({
+            name,
+            message
+        }))
+    })
+
     res.send({
         status: "success",
         message: message,
@@ -34,6 +44,82 @@ app.post("/message", (req, res) => {
 
 })
 
-app.listen("3000", () => {
+// app.listen("3000", () => {
+//     console.log("server is running in this port")
+// })
+// Replace app.listen with http.createServer
+
+
+//webSocket
+
+// we need to create a server manually
+const http = require("http");
+const server = http.createServer(app);
+
+server.listen("3000", () => {
     console.log("server is running in this port")
 })
+//Then create the WebSocket server:
+const wss = new WebSocket.Server({ server });
+/*✅ What this means
+wss = your WebSocket server
+It is connected to your HTTP server ✅
+It’s now ready to accept connections ✅
+*/
+wss.on("connection", (ws) => {
+    console.log("New client connected");
+    clients.push(ws);
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+        const index = clients.indexOf(ws);
+        clients.splice(index, 1);
+    });
+
+});
+//Each ws = one connected user
+//You detect when a user connects ✅
+//But you don’t remember them ❌
+
+
+
+//✅ STEP 3 — Store connected clients
+// const clients = [];
+
+//✅ STEP 4 — Handle disconnections (VERY important)
+/* ws.on("close", () => {
+        console.log("Client disconnected");
+
+        // remove this client from the array
+        const index = clients.indexOf(ws);
+        clients.splice(index, 1);
+    });
+✅ What this means (simple)
+
+ws.on("close") → runs when user leaves
+indexOf(ws) → find that user
+splice(...) → remove them
+
+✅ STEP 5 — Broadcast message to all clients
+Right now:
+
+You store messages ✅
+You track connected users ✅
+But you don’t send messages to them yet ❌
+✅ Goal of this step
+👉 When a new message is posted:
+
+Send it to every connected client instantly
+clients.forEach((client) => {
+    client.send(JSON.stringify({
+        name,
+        message
+    }));
+});
+``
+JSON.stringify → convert object → text (required)
+WebSockets only send strings, not objects
+
+
+*/
+
