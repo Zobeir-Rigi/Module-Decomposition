f87b360144191ef58c718f88e89482c9fbed4bec
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-06-25 01:39:02 +0100

    updated urls

--- chat-app/backend/app.js
+++ chat-app/backend/app.js
@@ -58,7 +58,7 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, () => {
     console.log("server is running");
 });
 
