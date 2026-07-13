49275cad85e26174d3f5a9140df5b2c1e7f4c49b
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-07-13 10:25:06 +0100

    deployed

--- chat-app/frontend/index.js
+++ chat-app/frontend/index.js
@@ -1,7 +1,7 @@
-// const BASE_URL = "https://pp3psp4mxrlip4uxvmeb9cmv.hosting.codeyourfuture.io"
-// const WS_URL = "wss://pp3psp4mxrlip4uxvmeb9cmv.hosting.codeyourfuture.io"
-const BASE_URL = "http://localhost:3000"
-const WS_URL = "ws://localhost:3000"
+const BASE_URL = "https://pp3psp4mxrlip4uxvmeb9cmv.hosting.codeyourfuture.io"
+const WS_URL = "wss://pp3psp4mxrlip4uxvmeb9cmv.hosting.codeyourfuture.io"
+// const BASE_URL = "http://localhost:3000"
+// const WS_URL = "ws://localhost:3000"
 
 const currentUser = localStorage.getItem("username");
 
